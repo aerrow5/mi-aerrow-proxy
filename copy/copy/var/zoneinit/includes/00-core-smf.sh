b5140d17cb95863d935b4f2b1c3a37304d6a0776
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-30 19:56:48 +0000

    Initial

---
+++ copy/copy/var/zoneinit/includes/00-core-smf.sh
@@ -0,0 +1,8 @@
+# This script load all SMF manifests from /opt/local/lib/svc/manifest
+
+CORE_SVC_DIR='/opt/local/lib/svc/manifest/'
+
+# temporary disables
+#for xml in ${CORE_SVC_DIR}*; do
+#	svccfg import ${xml}
+#done
---
+++ copy/copy/var/zoneinit/includes/10-delegated-dataset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/$UUID/data
+
+if ! zfs list $DDS > /dev/null; then
+        # No delegated dataset configured
+        exit 0
+fi
+
+zfs set mountpoint=/data $DDS
+zfs set compression=lz4 $DDS
+
+
+
