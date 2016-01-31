4cb454b7f5de0cde58a94143b7da50f33b639e64
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-31 19:57:14 +0100

    Delete 00-core-smf.sh

--- copy/var/zoneinit/includes/00-core-smf.sh
+++
@@ -1,8 +0,0 @@
-# This script load all SMF manifests from /opt/local/lib/svc/manifest
-
-CORE_SVC_DIR='/opt/local/lib/svc/manifest/'
-
-# temporary disables
-#for xml in ${CORE_SVC_DIR}*; do
-#	svccfg import ${xml}
-#done
